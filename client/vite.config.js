3982388d245de58fde9ad74f454d779150657e05
Sumlilili <tli148@my.centennialcollege.ca>
2025-07-30 01:43:33 -0400

    modify vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // or use '/your-subdirectory/' if not at root
+  build: {
+    emptyOutDir: true,
+    outDir: path.resolve(__dirname, '../dist/app') // ✅ Force build into dist/app in project root
+  }
 })
